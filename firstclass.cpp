9ea54584a80fdcb0f68cd914c77a24642679a42b
Adebanjo Favour Mayowa <favouradebanjo2@gmail.com>
2024-04-04 12:14:53 +0100

    First class writing C++

---
+++ firstclass.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(){
+    std::cout<<"Welcome to c++ class.This class promised to be exciting."<<std::endl;
+    std::cout<<"This is the first Class.It started late due to some technical Issues. n";
+    return 0;
+}
